21cff40c23e05e98edac96bcb88a0663f8bec3fc
Alexandra Goloskok <alexandra.goloskok_new@anstrex.com>
2022-12-23 11:04:40 +0200

    Added MUI

---
+++ src/components/auth/LoginFormComponent.js
@@ -0,0 +1,15 @@
+
+import Button from '@mui/material/Button';
+
+
+function LoginForm() {
+  return (
+    <div className="LoginForm">
+      <div>
+        <Button variant="contained">Hello World</Button>
+      </div>
+    </div>
+  );
+}
+
+export default LoginForm;
