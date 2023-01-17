3636cd6a230984da82c6532c58bdd22893290eaf
alexandraGoloskok1990 <alexandra.goloskok_new@anstrex.com>
2023-01-17 13:18:03 +0200

    Completed Login form without redux

--- src/components/auth/LoginComponent.js
+++ src/components/auth/LoginComponent.js
@@ -1,21 +1,71 @@
 import styles from './auth.module.css'
 import {Grid, TextField, Container, Button } from '@mui/material'
 import LoadingButton from '@mui/lab/LoadingButton'
-import { Form } from "react-router-dom"
+import {Form, useNavigate} from "react-router-dom"
 import { useForm, Controller } from "react-hook-form"
 import { Link } from "react-router-dom"
+import { config_main } from "../../config/config";
+import axios from "axios";
+import {useState} from "react";
 
 function LoginComponent() {
-    const { control, register, handleSubmit, formState: { errors } } = useForm({
+    const { control, register, handleSubmit, setError, formState: { errors } } = useForm({
       defaultValues: {
           email: '',
           password: ''
       }
     });
 
+    const [globalError, setGlobalError] = useState('')
+    const [globalSuccess, setGlobalSuccess] = useState('')
+    const [loading, setLoadingState] = useState(false)
+
+    const navigate = useNavigate();
+
+    const clearConditions = () => {
+        setGlobalSuccess('')
+        setGlobalError('')
+    }
+
     const onSubmit = data => {
-        console.log('data', data)
+        setLoadingState(true)
+        clearConditions()
+        axios.post(`${config_main.server_url_v1}/auth/login`, {...data})
+            .then(res => {
+                setGlobalSuccess('Sign In is successfully')
+                validateNotAuth('')
+                setLoadingState(false)
+                navigate('/')
+            }).catch(e => {
+                setLoadingState(false)
+                switch(e.response.status) {
+                    case 422:
+                        validateResponseFromServer(e.response.data.data)
+                        break
+                    case 401:
+                        validateNotAuth(e.response.data.message)
+                        break
+                    default:
+                        validateNotAuth('Error Sign in')
+                }
+            })
     };
+
+    const validateNotAuth = (message) => {
+        setGlobalError(message)
+    }
+
+    const validateResponseFromServer = (data) => {
+        if(Object.keys(data).length > 0) {
+            for(let item_key in data) {
+                let error_message = ''
+                for(let item_message_key in data[item_key]) {
+                    error_message += data[item_key][item_message_key]+' '
+                }
+                setError(item_key, { type: 'custom', message: error_message })
+            }
+        }
+    }
 
     const checkErrorHandle = (input) => {
         return errors[input] !== undefined ? true : false
@@ -36,6 +86,15 @@ function LoginComponent() {
                           <Grid item lg={12}>
                               <h1>Sign in</h1>
                           </Grid>
+
+                          {globalError !== '' && (
+                              <div className={styles.error}>{globalError}</div>
+                          )}
+
+                          {globalSuccess !== '' && (
+                              <div className={styles.success}>{globalSuccess}</div>
+                          )}
+
                           <Grid item lg={12}>
                               <Controller
                                   name="email"
@@ -77,7 +136,7 @@ function LoginComponent() {
                                   type="submit"
                                   fullWidth
                                   size="large"
-                                  loading={false}
+                                  loading={loading}
                                   variant="contained"
                               >Submit</LoadingButton>
                           </Grid>
---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+export const config_main = {
+    server_url_v1: 'http://api-google-ads.loc/api/v1',
+    server_url_v2: 'http://api-google-ads.loc/api/v2',
+}
